45f69e18819eca42e6f66ec38fc67456e10745b4
pbr142 <philipp.rindler@gmail.com>
2022-02-26 10:11:13 +0100

    build: get titanic data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='What does it really mean to "explain" a machine learning model?',
+    author='Philipp Rindler',
+    license='MIT',
+)
